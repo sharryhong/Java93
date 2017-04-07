99b530c37ede4964171b6e693b126029e640f547
sharryhong <kshopzoa15@gmail.com>
2017-04-07 15:47:53 +0900

    폴더삭제

--- javascript/step06/test08.js
+++
@@ -1,9 +0,0 @@
-/* 주제: 모듈. exports를 함수 객체로 대체하기
-*/
-
-"use strict"
-
-//
-var m1 = require('./test08_m1.js')
-// 
-m1()
--- javascript/step06/test08_m1.js
+++
@@ -1,6 +0,0 @@
-/* 주제: 모듈. exports를 함수 객체로 대체하기
-*/
-
-"use strict"
-
-module.exports = function() {console.log('hello')}
