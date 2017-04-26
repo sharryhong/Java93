bbdd20ccd5a455ada97a735b3e6c4b92a03f09ac
sharryhong <kshopzoa15@gmail.com>
2017-04-27 00:13:15 +0900

    폴더분리

--- javascript01/step13-hs/server.js
+++ javascript01/step13-hs/server.js
@@ -2,10 +2,6 @@ var miniExpress = require('./mini-express')
 
 var app = miniExpress()
 // 8888포트번호로 서버 시작.
-app.listen(8888, function() {
-  console.log('2. 서버 실행');
-})
-
 app.get('/get_test.do', function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'})
   response.write('GET 요청 처리!')
@@ -22,4 +18,7 @@ app.post('/post_test.do', function(request, response) {
   response.write('age=' + request.query.age)
   response.write('tel=' + request.query.tel)
   response.end()
+})
+app.listen(8888, function() {
+  console.log('2. 서버 실행');
 })
