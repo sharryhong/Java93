f29a36c2fab0a932e5e30568f6d7ac99f54126bd
sharryhong <kshopzoa15@gmail.com>
2017-04-26 22:58:11 +0900

    get() 추가

--- javascript01/step13-hs/mini-express.js
+++ javascript01/step13-hs/mini-express.js
@@ -1,7 +1,25 @@
 // nodejs http모듈 필요
 var http = require('http')
+var url = require('url')
+
+var getHandler = {}
+// 서버 생성 완료
 var server = http.createServer(function(request, response) {
-  response.end()
+  // method=get일 때 url query가 필요하기 때문에
+  var urlInfo = url.parse(request.url, true)
+  var handler = null
+  if (request.method == 'GET') { // 대문자
+    //
+    handler = getHandler[urlInfo.pathname]
+    // console.log('3' + handler)
+    // 처음엔 undefined,
+    // http://localhost:8888/들어갔을 때 handler에 test.js의 두번째 아규먼트인 함수
+    request.query = urlInfo.query
+  }
+  if (handler) { // http://localhost:8888/들어갔을 때 실행
+    console.log('???')
+    handler(request, response)
+  }
 })
 
 module.exports = function() {
@@ -9,6 +27,11 @@ module.exports = function() {
     listen(port, cb) {
       // nodejs http : server.listen
       server.listen(port, cb)
+    },
+    get(url, cb) {
+      // console.log('1' + url + cb); // test.js의 아규먼트들이 잘들어옴
+      getHandler[url] = cb
+      // test.js의 두번째 아규먼트인 함수가 getHandler객체의 url(/) 프로퍼티 값으로 됨
     }
   }
 }
--- javascript01/step13-hs/test.js
+++ javascript01/step13-hs/test.js
@@ -3,5 +3,14 @@ var miniExpress = require('./mini-express')
 var app = miniExpress()
 // 8888포트번호로 서버 시작.
 app.listen(8888, function() {
-  console.log('서버 실행');
+  console.log('2. 서버 실행');
+})
+
+app.get('/', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'})
+  response.end('<html> \
+  <body> \
+  <h1>안녕하세요!</h1> \
+  </body> \
+  </html>')
 })
