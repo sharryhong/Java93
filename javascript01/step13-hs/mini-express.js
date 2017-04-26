e0ea28f98bc795b934f87bf21eed436b47fb95f1
sharryhong <kshopzoa15@gmail.com>
2017-04-27 00:08:17 +0900

    post요청 추가

--- javascript01/step13-hs/mini-express.js
+++ javascript01/step13-hs/mini-express.js
@@ -3,9 +3,13 @@ var http = require('http')
 var url = require('url')
 var path = require('path')
 var fs = require('fs')
-
+// post요청시 querystring모듈 필요
+var qs = require('querystring')
+// get요청일 때 담을 객체
 var getHandler = {}
-// 없는 url일 때 
+// post요청일 때 담을 객체
+var postHandler = {}
+// 없는 url일 때
 function notFound(request, response) {
   response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'})
   fs.readFile(path.join(__dirname, './html/http-404.html'), function(err, data) {
@@ -13,22 +17,44 @@ function notFound(request, response) {
     response.end(data)
   })
 }
+// 함수로 코드 분리
+function findHandler(method, servicePath) {
+  // console.log(method + servicePath);
+  if (method == 'GET') {
+    // getHandler 객체에 프로퍼티 추가
+    return getHandler[servicePath]
+  } else if (method == 'POST') {
+    return getHandler[servicePath]
+  }
+  return null
+}
+
 // 서버 생성 완료
 var server = http.createServer(function(request, response) {
   // method=get일 때 url query가 필요하기 때문에
   var urlInfo = url.parse(request.url, true)
-  var handler = null
-  if (request.method == 'GET') { // 대문자
-    //
-    handler = getHandler[urlInfo.pathname]
-    // console.log('3' + handler)
-    // 처음엔 undefined,
-    // http://localhost:8888/들어갔을 때 handler에 test.js의 두번째 아규먼트인 함수
-    request.query = urlInfo.query
-  }
-  if (handler) { // http://localhost:8888/들어갔을 때 실행
-    // test.js의 두번째 아규먼트인 함수에 아규먼트 값 줌
-    handler(request, response)
+  var handler = findHandler(request.method, urlInfo.pathname)
+  // console.log('..'+findHandler(request.method, urlInfo.pathname));
+  if (handler) {
+    if (request.method == 'GET') {
+      // console.log('getHandler:' +getHandler[urlInfo.pathname]);
+      request.query = urlInfo.query
+      handler(request, response)
+  /* getHandler = {
+      [urlInfo.pathname] : function(request, response) {...} => test.js의 두번째 아규먼트인 함수가 들어있다. 그걸 실행시켜라.
+    }
+  */
+    } else if (request.method == 'POST') {
+      var queryString = ''
+      request.on('data', function(data) {
+        queryString += data
+      })
+      // 쿼리스트링 데이터 다 받으면 실행
+      request.on('end', function() {
+        request.query = qs.parse(queryString)
+        handler(request, response)
+      })
+    }
   } else {
     notFound(request, response)
   }
@@ -36,14 +62,17 @@ var server = http.createServer(function(request, response) {
 
 module.exports = function() {
   return {
-    listen(port, cb) {
-      // nodejs http : server.listen
-      server.listen(port, cb)
-    },
     get(url, cb) {
       // console.log('1' + url + cb); // test.js의 아규먼트들이 잘들어옴
       getHandler[url] = cb
       // test.js의 두번째 아규먼트인 함수가 getHandler객체의 url(/) 프로퍼티 값으로 됨
+    },
+    post(url, cb) {
+      getHandler[url] = cb
+    },
+    listen(port, cb) {
+      // nodejs http : server.listen
+      server.listen(port, cb)
     }
   }
 }
--- javascript01/step13-hs/test.js
+++ javascript01/step13-hs/test.js
@@ -6,11 +6,20 @@ app.listen(8888, function() {
   console.log('2. 서버 실행');
 })
 
-app.get('/', function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'})
-  response.end('<html> \
-  <body> \
-  <h1>안녕하세요!</h1> \
-  </body> \
-  </html>')
+app.get('/get_test.do', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'})
+  response.write('GET 요청 처리!')
+  response.write('name=' + request.query.name)
+  response.write('age=' + request.query.age)
+  response.write('tel=' + request.query.tel)
+  response.end()
+})
+
+app.post('/post_test.do', function(request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain;charset=UTF-8'})
+  response.write('POST 요청 처리!')
+  response.write('name=' + request.query.name)
+  response.write('age=' + request.query.age)
+  response.write('tel=' + request.query.tel)
+  response.end()
 })
