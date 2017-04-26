f8e206e1a85e192b675fc6d19f918c4727945806
sharryhong <kshopzoa15@gmail.com>
2017-04-26 22:22:54 +0900

    서버실행을 위해 http모듈 필요

--- javascript01/step13-hs/mini-express.js
+++ javascript01/step13-hs/mini-express.js
@@ -1,3 +1,9 @@
+// nodejs http모듈 필요
+var http = require('http')
+var server = http.createServer(function(request, response) {
+  response.end()
+})
+
 module.exports = function() {
   return {
     listen(port, cb) {
