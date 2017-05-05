80f0c996644b965c098a5d748ab880ef8fc8b95b
sharryhong <kshopzoa15@gmail.com>
2017-05-05 21:48:28 +0900

    숫자 넣는방법 수정

--- electron01/test02-sliding-puzzle/sliding-puzzle.js
+++ electron01/test02-sliding-puzzle/sliding-puzzle.js
@@ -4,14 +4,22 @@ window.$ = window.jQuery = require('jquery')
 let xCol = 4, // 행의 갯수
     yRow = 4, // 열의 갯수
     numArray = [], // 숫자들 담을 배열
+    blankEl = '', // 빈칸 엘리먼트
     puzzleTable = $('#puzzle-table');
+
+makeNumArray()
 
 // 배열에 숫자들 담는 함수
 function makeNumArray() {
+  var startNum = 1
+  var totalNum = xCol * yRow
   for (let i = 0; i < xCol; i++) {
     numArray[i] = new Array()  // 2차원배열로 만들기위해
     for (let j = 0; j < yRow; j++) {
-      numArray[i][j] = i * xCol + j + 1 // 숫자 넣기
+      numArray[i][j] = startNum++ // 숫자 넣기
+      if (startNum - 1 == totalNum) {
+        numArray[i][j] = '' // 마지막 숫자에 빈칸 넣기
+      }
     }
   }
   displayNum()
@@ -29,5 +37,3 @@ function displayNum() {
   }
   puzzleTable.html(html)
 }
-
-makeNumArray()
