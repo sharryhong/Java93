7ac08abff3f9afd0e37bc8b52e93f8d7efd98f1a
sharryhong <kshopzoa15@gmail.com>
2017-05-06 23:20:22 +0900

    위, 오른쪽, 아래, 왼쪽 엘리먼트만 클릭이 되도록

--- electron01/test02-sliding-puzzle/sliding-puzzle.js
+++ electron01/test02-sliding-puzzle/sliding-puzzle.js
@@ -4,7 +4,11 @@ window.$ = window.jQuery = require('jquery')
 let xCol = 4, // 행의 갯수
     yRow = 4, // 열의 갯수
     numArray = [], // 숫자들 담을 배열
+    nums = null, // 숫자버튼 엘리먼트
     blankEl = null, // 빈칸 엘리먼트
+    blankX = 0,
+    blankY = 0,
+    canClickEl = [], // 클릭할 수 있는 엘리먼트
     puzzleTable = $('#puzzle-table');
 
 makeNumArray()
@@ -32,14 +36,59 @@ function displayNum() {
     html += '<tr>'
     for (let j = 0; j < yRow; j++) {
       if (numArray[i][j] == '') {
-        html += '<td id="blank">' + numArray[i][j] + '</td>'
+        html += '<td id="blank" class="btn" data-x="'+ j + '" data-y="' + i + '">' + numArray[i][j] + '</td>'
       } else {
-        html += '<td class="num">' + numArray[i][j] + '</td>'
+        html += '<td class="btn num" data-x="'+ j + '" data-y="' + i + '">' + numArray[i][j] + '</td>'
       }
     }
     html += '</tr>'
-    blankEl = $('#blank')
-    console.log(blankEl);
   }
   puzzleTable.html(html)
+
+  // 빈칸 엘리먼트의 위, 오른쪽, 아래, 왼쪽 엘리먼트만 클릭이 되도록하기
+  blankEl = $('#blank')
+  blankX = blankEl.attr("data-x")
+  blankY = blankEl.attr("data-y")
+  nums = $('.num')
+  for (let el of nums) {
+    if (el.getAttribute("data-x") == blankX && el.getAttribute("data-y") == blankY - 1) { // 위
+      el.classList.add('can-click')
+      canClickEl.push(el)
+    }
+    if (el.getAttribute("data-x") == blankX - 1 && el.getAttribute("data-y") == blankY) { // 왼쪽
+      el.classList.add('can-click')
+      canClickEl.push(el)
+    }
+  }
+
+  canClickEl.push(blankEl[0]) // 빈칸도 추가해야 제대로 동작한다.
+  for (let el of canClickEl) {
+    el = $(el) // 일반 DOM 엘리먼트를 jQuery 엘리먼트로
+    el.click(function() {
+      var firstNum = $(this)
+      var txt = parseInt(firstNum.text())
+      blankEl.removeAttr('id').addClass('num can-click').html(txt)
+      firstNum.removeClass('num can-click').text('').attr('id', 'blank')
+      blankEl = firstNum
+    })
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
