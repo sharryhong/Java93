15cbeac90fd550b55f8bc9d2b490dfcd77cecb0f
sharryhong <kshopzoa15@gmail.com>
2017-05-07 16:28:15 +0900

    반복코드 함수로 묶기

--- electron01/test02-sliding-puzzle/sliding-puzzle.js
+++ electron01/test02-sliding-puzzle/sliding-puzzle.js
@@ -50,59 +50,8 @@ function displayNum() {
 
 }
 
-// 빈칸 엘리먼트의 위, 오른쪽, 아래, 왼쪽 엘리먼트만 클릭이 되도록하기
-function clickBtn() {
-  blankEl = $('#blank')
-  blankX = parseInt(blankEl.attr("data-x"))
-  blankY = parseInt(blankEl.attr("data-y"))
-  btns = $('.btn')
-  for (let el of btns) {
-    el = $(el)
-    console.log(el);
-    if (el.attr("data-x") == blankX && el.attr("data-y") == blankY - 1) { // 위
-      el.addClass('can-click')
-    }
-    else if (el.attr("data-x") == blankX - 1 && el.attr("data-y") == blankY) { // 왼쪽
-      el.addClass('can-click')
-    }
-    else if (el.attr("data-x") == blankX + 1 && el.attr("data-y") == blankY) { // 오른쪽
-      el.addClass('can-click')
-    }
-    else if (el.attr("data-x") == blankX && el.attr("data-y") == blankY + 1) { // 아래
-      el.addClass('can-click')
-    } else {
-      el.removeClass('can-click')
-    }
-  }
-  canClickEl = $('.can-click')
-  // canClickEl = null
-  // canClickEl = $('.can-click')
-  // class="can-click" 인 녀석들만 클릭되게 하기
-  // setClick = canClickEl.click(function() {
-  //   console.log(canClickEl);
-  //   exchange($(this))
-  // })
-  btns.click(function() {
-    // console.log(canClickEl);
-    exchange($(this))
-  })
-}
-
-// 빈칸과 클릭한 숫자 바꾸기
-function exchange(thisEl) {
-  console.log('ok');
-  // 자꾸 기존 class="can-click" 이었던 놈들도 클릭이 되어 조건문을 두었다.
-  // canClickEl.click()이 메모리에 남아있는 것같다. clear할 수 있는 방법은?
-  if ( (thisEl.attr("data-x") == blankX && thisEl.attr("data-y") == blankY - 1) ||
-        (thisEl.attr("data-x") == blankX - 1 && thisEl.attr("data-y") == blankY) ||
-        (thisEl.attr("data-x") == blankX + 1 && thisEl.attr("data-y") == blankY) ||
-        (thisEl.attr("data-x") == blankX && thisEl.attr("data-y") == blankY + 1) ) {
-    var txt = parseInt(thisEl.text())
-    blankEl.removeAttr('id').addClass('num can-click').html(txt)
-    thisEl.removeClass('num can-click').text('').attr('id', 'blank')
-    blankEl = thisEl
-  }
-  // canClick()
+// 빈칸 엘리먼트의 위, 오른쪽, 아래, 왼쪽 엘리먼트만 class="can-click"
+function canClick() {
   blankEl = $('#blank')
   blankX = parseInt(blankEl.attr("data-x"))
   blankY = parseInt(blankEl.attr("data-y"))
@@ -128,10 +77,29 @@ function exchange(thisEl) {
   canClickEl = $('.can-click')
 }
 
+// 빈칸과 클릭한 숫자 바꾸기
+function clickBtn() {
+  canClick() // 초기 한번 셋팅
+  btns.click(function() {
+    console.log('ok');
+    var thisEl = $(this)
+    // 상하좌우만
+    if ( (thisEl.attr("data-x") == blankX && thisEl.attr("data-y") == blankY - 1) ||
+          (thisEl.attr("data-x") == blankX - 1 && thisEl.attr("data-y") == blankY) ||
+          (thisEl.attr("data-x") == blankX + 1 && thisEl.attr("data-y") == blankY) ||
+          (thisEl.attr("data-x") == blankX && thisEl.attr("data-y") == blankY + 1) ) {
+      var txt = parseInt(thisEl.text())
+      blankEl.removeAttr('id').addClass('num can-click').html(txt)
+      thisEl.removeClass('num can-click').text('').attr('id', 'blank')
+      blankEl = thisEl
+    }
+    canClick()
+  })
+}
+
 // 섞기
 $('.mix').click(function() {
   var no = parseInt(Math.random() * canClickEl.length)
-  console.log(no);
   canClickEl = canClickEl[no]
   canClickEl.click(function() {
     exchange($(this))
