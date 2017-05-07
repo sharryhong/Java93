aae9d07c1b354ec2bbf489f14a704d9931d5f3f2
sharryhong <kshopzoa15@gmail.com>
2017-05-07 16:21:16 +0900

    스타일부분과 데이터부분 분리

--- electron01/test02-sliding-puzzle/sliding-puzzle.js
+++ electron01/test02-sliding-puzzle/sliding-puzzle.js
@@ -46,34 +46,35 @@ function displayNum() {
   }
   puzzleTable.html(html)
 
-  canClick()
+  clickBtn()
 
 }
 
 // 빈칸 엘리먼트의 위, 오른쪽, 아래, 왼쪽 엘리먼트만 클릭이 되도록하기
-function canClick() {
+function clickBtn() {
   blankEl = $('#blank')
   blankX = parseInt(blankEl.attr("data-x"))
   blankY = parseInt(blankEl.attr("data-y"))
   btns = $('.btn')
-  // for (let el of btns) {
-  //   el = $(el)
-  //   console.log(el);
-  //   if (el.attr("data-x") == blankX && el.attr("data-y") == blankY - 1) { // 위
-  //     el.addClass('can-click')
-  //   }
-  //   else if (el.attr("data-x") == blankX - 1 && el.attr("data-y") == blankY) { // 왼쪽
-  //     el.addClass('can-click')
-  //   }
-  //   else if (el.attr("data-x") == blankX + 1 && el.attr("data-y") == blankY) { // 오른쪽
-  //     el.addClass('can-click')
-  //   }
-  //   else if (el.attr("data-x") == blankX && el.attr("data-y") == blankY + 1) { // 아래
-  //     el.addClass('can-click')
-  //   } else {
-  //     el.removeClass('can-click')
-  //   }
-  // }
+  for (let el of btns) {
+    el = $(el)
+    console.log(el);
+    if (el.attr("data-x") == blankX && el.attr("data-y") == blankY - 1) { // 위
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX - 1 && el.attr("data-y") == blankY) { // 왼쪽
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX + 1 && el.attr("data-y") == blankY) { // 오른쪽
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX && el.attr("data-y") == blankY + 1) { // 아래
+      el.addClass('can-click')
+    } else {
+      el.removeClass('can-click')
+    }
+  }
+  canClickEl = $('.can-click')
   // canClickEl = null
   // canClickEl = $('.can-click')
   // class="can-click" 인 녀석들만 클릭되게 하기
@@ -87,12 +88,9 @@ function canClick() {
   })
 }
 
-
-
 // 빈칸과 클릭한 숫자 바꾸기
 function exchange(thisEl) {
   console.log('ok');
-  setClick = null
   // 자꾸 기존 class="can-click" 이었던 놈들도 클릭이 되어 조건문을 두었다.
   // canClickEl.click()이 메모리에 남아있는 것같다. clear할 수 있는 방법은?
   if ( (thisEl.attr("data-x") == blankX && thisEl.attr("data-y") == blankY - 1) ||
@@ -109,6 +107,25 @@ function exchange(thisEl) {
   blankX = parseInt(blankEl.attr("data-x"))
   blankY = parseInt(blankEl.attr("data-y"))
   btns = $('.btn')
+  for (let el of btns) {
+    el = $(el)
+    // console.log(el);
+    if (el.attr("data-x") == blankX && el.attr("data-y") == blankY - 1) { // 위
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX - 1 && el.attr("data-y") == blankY) { // 왼쪽
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX + 1 && el.attr("data-y") == blankY) { // 오른쪽
+      el.addClass('can-click')
+    }
+    else if (el.attr("data-x") == blankX && el.attr("data-y") == blankY + 1) { // 아래
+      el.addClass('can-click')
+    } else {
+      el.removeClass('can-click')
+    }
+  }
+  canClickEl = $('.can-click')
 }
 
 // 섞기
