08e9f3177f08c74e5fcdd8227cc13522567ed79c
sharryhong <kshopzoa15@gmail.com>
2017-05-25 23:36:29 +0900

    서버접속시 환영인사

--- electron01/step16-hs/index.js
+++ electron01/step16-hs/index.js
@@ -26,23 +26,22 @@ var buffer = ''
 	
 // 서버에서 data받으면
 socket.on('data', (data) => {
-//	buffer += data.toString() // 데이터 올때마다 버퍼에 담는다. 
-//    var value = null
-	// 줄바꿈 코드를 찾아서 값 추출하기
-//	while (true) {
-//		var newLineIndex = buffer.indexOf('\n')
-//		if (newLineIndex < 0) return; // 줄바꿈코드가 없다면 : 아직 데이터를 덜 받았다.
-		// 줄바꿈 코드가 있다면 예) aaa\nccc\n
-		// \n코드 앞 문자열 추출 
-//		value = buffer.substring(0, newLineIndex)
-		// \n코드 뒤 문자열은 다시 버퍼에 담기 
-//		buffer = buffer.substring(newLineIndex + 1)		
-//		if (value.length > 0) break; // \n코드 앞 문자열이 있으면 반복문을 멈춰서 처리
-//	}
-//	console.log(value)	
-//	showMsg.val(value)
-//	showMsg.val(data.toString())
+	buffer += data.toString() // 데이터 올때마다 버퍼에 담는다. 
+    var value = null
+//	 줄바꿈 코드를 찾아서 값 추출하기
+	while (true) {
+		var newLineIndex = buffer.indexOf('\n')
+		if (newLineIndex < 0) return; // 줄바꿈코드가 없다면 : 아직 데이터를 덜 받았다.
+//		 줄바꿈 코드가 있다면 예) aaa\nccc\n
+//		 \n코드 앞 문자열 추출 
+		value = buffer.substring(0, newLineIndex)
+//		 \n코드 뒤 문자열은 다시 버퍼에 담기 
+		buffer = buffer.substring(newLineIndex + 1)		
+		if (value.length > 0) break; // \n코드 앞 문자열이 있으면 반복문을 멈춰서 처리
+	}
+	console.log(value)	
 	showMsg.append("<p>"+ data.toString() +"</p>")
+	showMsg[0].scrollTop = showMsg[0].scrollHeight;
 })
 
 socket.on('error', (error) => {
@@ -55,7 +54,11 @@ socket.on('close', () => {
 })
 
 connectBtn.click(() => {
+	var objConnect = {
+		nickname: nickname.val()
+	}
 	socket.connect(parseInt(port.val()), host.val())
+	socket.write(JSON.stringify(objConnect) + '\n')
 })
 
 closeBtn.click(() => {
@@ -65,10 +68,8 @@ closeBtn.click(() => {
 	closeBtn.css('display', 'none')
 })
 
-var obj = null;
-
 $('#sendBtn').click(() => {
-	obj = {
+	var obj = {
 		msgBox: msgBox.val(),
 		nickname: nickname.val()
 	}
